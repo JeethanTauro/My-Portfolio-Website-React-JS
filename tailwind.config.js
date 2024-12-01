a8989b81040e895f49a1f481c5786048b505f1ae
JeethanTauro <tauroshanjeeth@gmail.com>
2024-12-01 15:59:42 +0530

    Added a favicon and changed the title of the document

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,14 +8,20 @@ export default {
     extend: {
       animation: {
         fall: 'fall 10s linear infinite',
+        fadeIn : 'fadeIn 0.5s ease-out',
       },
       keyframes: {
         fall: {
           '0%': { backgroundPosition: '0 0' },
           '100%': { backgroundPosition: '0 100%' },
         },
+        fadeIn: {
+          '0%': { opacity: '0', transform: 'translateY(20px)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' },
+        },
       },
     },
+    
   },
   plugins: [
     
